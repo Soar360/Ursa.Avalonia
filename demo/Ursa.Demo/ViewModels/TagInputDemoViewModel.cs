2f288764c896ebbc1c47bd10b8b6e423f6b32bb9
Soar360 <Soar360@live.com>
2024-11-15 13:31:29 +0800

    LoadTagsCommand

--- demo/Ursa.Demo/ViewModels/TagInputDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/TagInputDemoViewModel.cs
@@ -1,10 +1,14 @@
+using Avalonia.Threading;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Ursa.Demo.ViewModels;
 
-public class TagInputDemoViewModel: ViewModelBase
+public class TagInputDemoViewModel : ViewModelBase
 {
-    private ObservableCollection<string> _tags = new () ;
+    private ObservableCollection<string> _tags = new();
     public ObservableCollection<string> Tags
     {
         get => _tags;
@@ -16,5 +20,21 @@ public class TagInputDemoViewModel: ViewModelBase
     {
         get => _distinctTags;
         set => SetProperty(ref _distinctTags, value);
+    }
+
+    public ICommand LoadTagsCommand { get; set; }
+    private async Task LoadTags()
+    {
+        await Task.Delay(1000);
+
+        for (int i = 0; i < 5; i++)
+        {
+            this.Tags.Add("Tag" + i);
+        }
+    }
+
+    public TagInputDemoViewModel()
+    {
+        this.LoadTagsCommand = new AsyncRelayCommand(LoadTags);
     }
 }
